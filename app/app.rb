75fd528496d73c5ec34d10e04627ba94c8b49e92
Paul Harker <harkerp@gmail.com>
2015-06-29 17:56:48 +0100

    Houston, we have green tests

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,17 @@
 require 'sinatra/base'
 require './app/app'
+require 'data_mapper'
 
 
-class AppWeb < Sinatra::Base 
+class AppWeb < Sinatra::Base
+  run! if app_file == $0
+  set :views, proc { File.join(root, '..', 'views') }
+
+
+   get '/links' do
+     @links = Link.all
+     erb :'links/index'
+   end
+
+
 end
